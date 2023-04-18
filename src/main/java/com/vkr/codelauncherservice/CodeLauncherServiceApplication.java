d713837e7b76f9ca889ed5b7668d7e2bd7b306b4
aluk0220 <aleksandr.ukhov@netcracker.com>
2023-04-19 00:59:13 +0400

    Initial commit

---
+++ src/main/java/com/vkr/codelauncherservice/CodeLauncherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vkr.codelauncherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeLauncherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeLauncherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vkr/codelauncherservice/CodeLauncherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkr.codelauncherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLauncherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
